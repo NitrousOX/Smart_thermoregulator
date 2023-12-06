0dea8324d065ef7ff48c910614c70edafdb5372f
NitrousOX <poljvasdavid@gmail.com>
2023-12-06 19:10:19 +0100

    tempSlanje
    slanje temp i device klasa

---
+++ Smart_termoregulator/Smart_termoregulator/src/Device/Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_termoregulator.src.Device
+{
+    public class Device
+    {
+        int id;
+        float temperatura;
+
+        Device(float temperatura) {
+            this.id = new Random().Next(1, 10000);
+            this.Temperatura = temperatura;
+        }
+
+        public int ID { get => id; set => id = value; }
+        public float Temperatura { get => temperatura; set => temperatura = value; }
+    }
+}
---
+++ Smart_termoregulator/Smart_termoregulator/src/Device/deviceSlanje.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_termoregulator.src.Device
+{
+    public class deviceSlanje : IslanjeTemperature
+    {
+        public void PosaljiTemperaturu()
+        {
+            
+        }
+    }
+}
