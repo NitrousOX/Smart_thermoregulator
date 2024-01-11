82339e61473e4cd4e5c43f6bdc795b6238604df3
NitrousOX <poljvasdavid@gmail.com>
2024-01-11 22:43:56 +0100

    commit

---
+++ Smart_termoregulator/TestProject/HeaterTests/HeaterTest.cs
@@ -0,0 +1,25 @@
+﻿using Smart_termoregulator.src.Heater.implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.Heater
+{
+    
+    public class HeaterTest
+    {
+        [Fact]
+        public void heaterPromenaStanja_Upali()
+        {
+            //Arrange - variables
+
+            //ACT
+            
+
+            //Assert
+            
+        }
+    }
+}
---
+++ Smart_termoregulator/TestProject/RegulatorTests/RegulatorTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.RegulatorTests
+{
+    public class RegulatorTest
+    {
+        [Fact]
+        public void regulatorTesting()
+        {
+            //Arrange
+            
+            //Act
+           
+            //Assert
+        }
+    }
+}
