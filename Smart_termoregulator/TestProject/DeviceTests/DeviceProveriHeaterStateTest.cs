fc7cf2b26191631f3be45d9718c1031d61e7937a
leaaa444 <leasusic2002@gmail.com>
2024-01-11 23:26:12 +0100

    testovi device

---
+++ Smart_termoregulator/TestProject/DeviceTests/DeviceProveriHeaterStateTest.cs
@@ -0,0 +1,27 @@
+﻿using Smart_termoregulator.src.Device.implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject.DeviceTests
+{
+    public class DeviceProveriHeaterStateTest
+    {
+        [Fact]
+        public void proveriHeaterStateTesting()
+        {
+            //Arrange - variables
+            int id = 1;
+            Device device = new Device(id);
+            DeviceProveriHeaterState ProveriHeaterState = new DeviceProveriHeaterState();
+
+            //ACT
+            ProveriHeaterState.proveriHeaterState(device);
+            
+            //Assert
+
+        }
+    }
+}
